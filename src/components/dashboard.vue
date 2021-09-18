f21433e510d15ad75351b6ba0206c712008cde7d
Emilsone <lawsonezekiel68@gmail.com>
2021-09-18 13:24:29 -0700

    create components

---
+++ src/components/dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex ">
+    <SideBar />
+
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <NavLink />
+
+      <main class="flex-1 overflow-x-hidden overflow-y-auto">
+        <div class="container mx-auto px-6 py-8">
+          <router-view></router-view>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavLink from "@/components/NavLink.vue";
+import SideBar from "./side-bar/side-bar.vue";
+// import overview from "@/components/overview.vue";
+export default {
+  name: "Dashboard",
+  components: { SideBar, NavLink },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import Router from "vue-router";
+// import BoardPage from "@/components/BoardPage.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  base: "/",
+  mode: "history",
+  routes: [
+    // {
+    //   path: "/schedule",
+    //   name: "SchedulePage",
+    //   component: SchedulePage,
+    // },
+    // {
+    //   path: "/links/:id",
+    //   component: LinkDetails,
+    //   children: [
+    //     {
+    //       path: "edit",
+    //       component: EditLink,
+    //     },
+    //   ],
+    // },
+    
+  ],
+});
